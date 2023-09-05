0b2cbb6b116016d92910acd4b82e442e844baef6
Monster <1264833435@qq.com>
2023-09-05 16:26:26 +0800

    feat: 添加接口注释

--- src/main/java/com/github/monster/controller/CollectController.java
+++ src/main/java/com/github/monster/controller/CollectController.java
@@ -5,6 +5,9 @@ import com.github.monster.core.pool.DynamicTaskPool;
 import com.github.monster.core.task.Task;
 import com.github.monster.core.util.SpringContextUtil;
 import com.github.monster.service.AttackCityService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.Date;
@@ -13,12 +16,14 @@ import java.util.Date;
  * @author Monster
  * @date 2023/9/5 15:00
  */
+@RestController
+@RequestMapping()
 public class CollectController {
 
     @Resource
     private DynamicTaskPool dynamicTaskPool;
 
-
+    @GetMapping("/test")
     public void startAttackCityStatistics() {
 
         Date startTime = DateUtil.offsetMillisecond(new Date(), 10).toCalendar().getTime();
